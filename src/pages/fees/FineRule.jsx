a9b9333b5a108d390a5eba56d2d05f9ebf72220f
Gulshankuamr <gulshan73939314@gmail.com>
2026-01-26 17:18:34 +0530

    Fix feeFineRuleService import path and casing

--- src/pages/fees/FineRule.jsx
+++ src/pages/fees/FineRule.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'; 
 import { FiEdit2, FiTrash2, FiSave, FiX, FiSearch, FiCheckCircle, FiList } from 'react-icons/fi';
 import { HiOutlineChevronLeft, HiOutlineChevronRight } from 'react-icons/hi';
-import { feeFineRuleService } from '../../services/FeeFineRuleService';
+import { feeFineRuleService } from '../../services/feeFineRuleService';
 import { feeHeadService } from '../../services/feeHeadService';
 
 const FineRule = () => {
